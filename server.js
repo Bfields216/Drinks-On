d413abc20c2063566145a6986de0087cabe8799d
Mia112 <msheriff112@gmail.com>
2019-11-17 21:06:51 -0500

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 const config = require('config');
+const path = require('path');
 
 
 
@@ -16,7 +17,7 @@ const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
-  .connect(db, { 
+  .connect(db,  { 
     useNewUrlParser: true,
     useCreateIndex: true
   }) // Adding new mongo url parser
@@ -34,10 +35,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
