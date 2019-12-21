fe474e083734bf55a6ad87c65d51b530c3e81825
Miatta Sheriff <msheriff112@gmail.com>
2019-12-20 22:17:01 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,35 +19,8 @@ import { loadUser } from './actions/authActions';
 
 import Home from "./containers/Home";
 
-
-// import Register from "./components/auth/Register";
-// import Login from "./components/auth/Login";
-
-// import Navbar from "./components/Navbars/Navbar";
-
-// import TopNav from "./components/TopNavbar";
-
 import "./index.css";
 
-// // Check for token to keep user logged in
-// if (localStorage.jwtToken) {
-//   // Set auth token header auth
-//   const token = localStorage.jwtToken;
-//   setAuthToken(token);
-//   // Decode token and get user info and exp
-//   const decoded = jwt_decode(token);
-//   // Set user and isAuthenticated
-//   store.dispatch(setCurrentUser(decoded));
-//   // Check for expired token
-//   const currentTime = Date.now() / 1000; // to get in milliseconds
-//   if (decoded.exp < currentTime) {
-//     // Logout user
-//     store.dispatch(logoutUser());
-
-//     // Redirect to login
-//     window.location.href = "./login";
-//   }
-// }
 class App extends Component {
   componentDidMount() {
     store.dispatch(loadUser());
