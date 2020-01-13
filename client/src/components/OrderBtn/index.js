0f38040f82f8cd9bae030eefdad145ff8448e840
Miatta Sheriff <msheriff112@gmail.com>
2020-01-13 14:09:25 -0500

    Update index.js

--- client/src/components/OrderBtn/index.js
+++ client/src/components/OrderBtn/index.js
@@ -1,34 +1,7 @@
-// import React from "react";
-
-
-// // Using the datalist element we can create autofill suggestions based on the props.breeds array
-// function OrderBtn(props) {
-//   return (
-//     <div>
-//       <a href="/summary">
-//         <button
-//           type="button"
-//           className="general-btn btn-lg btn-block"
-//           onClick={props.createOrder}
-//         >
-//           View Order Summery
-//         </button>
-//       </a>
-//     </div>
-//   );
-// }
-
-// export default OrderBtn;
-
-
 import React from "react";
-// import "./style.css";
-
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
 function OrderBtn(props) {
   return (
     <div>
-      {/* <a href="/summary"> */}
         <button
           type="button"
           className="general-btn btn-lg"
@@ -36,11 +9,9 @@ function OrderBtn(props) {
         >
           View Order Summery
         </button>
-      {/* </a> */}
     </div>
   );
 }
-
 export default OrderBtn;
 
 
